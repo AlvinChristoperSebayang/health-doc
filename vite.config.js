3300b89c04c9a98eabd37d37427e44b790a0d5ce
AlvinChristoperSebayang <csa7045@gmail.com>
2025-04-22 16:33:27 +0700

    add route

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html',
+                contact: 'contact.html',
+                blog: 'blog.html',
+                single: 'single.html',
+            }
+        }
+    }
+}
